280f4ec881050d2c8e371d1ecd733e25673ce37f
unknown <ballogggamer@gmail.com>
2024-01-24 16:32:19 -0300

    implemented sorting for css

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 const http = require("http")
 const {readPath} = require("./readFile")
+const {findFileExtension} = require("./findFileExtension")
 const port = 2020
 
 http.createServer(function (request,response) {
-    switch (request.url) {
+    let Path = request.url
+    let fileExtension = findFileExtension(Path) 
+
+    switch (fileExtension) {
+        case "css":
+            response.writeHead(200, {"Content-Type":"text/css"})
+            response.write(readPath("."+Path))
+            response.end()
+            break;
         default: 
             response.writeHead(200, {"Content-Type":"text/html"})
             response.write(readPath("./index.html"))
