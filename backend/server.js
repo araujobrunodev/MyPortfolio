2c4075767e359d983adedfd630859a9ff9135fe6
unknown <ballogggamer@gmail.com>
2024-01-24 13:48:31 -0300

    send index file to browser

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const http = require("http")
+const {readPath} = require("./readFile")
 const port = 2020
 
 http.createServer(function (request,response) {
-    response.writeHead(200, {"Content-Type":"text/html"})
-    response.write("<h1>ok<h1>")
-    response.end()
+    switch (request.url) {
+        default: 
+            response.writeHead(200, {"Content-Type":"text/html"})
+            response.write(readPath("./index.html"))
+            response.end()
+            break;
+    }
 }).listen(port)
 
 console.log(`http://localhost:${port}`)
