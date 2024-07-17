eb9c3e57234fb51598446e73f7444cee64402151
unknown <ballogggamer@gmail.com>
2024-07-17 20:57:18 -0300

    add about-me section

--- effect/changeUrl.js
+++ effect/changeUrl.js
@@ -1,11 +1,5 @@
-const semicircle = document.getElementById("semicircle")
-const topLines = document.getElementById("topLines")
-const background = document.getElementById("section2_5")
 const lines = document.querySelector(".line-down")
 
 if (window.screen.width <= 500) {
-    semicircle.setAttribute("src","./images/semicircle2.svg")
-    topLines.setAttribute("src","./images/topLines2.svg")
-    background.style.backgroundImage = "url(./images/background4.svg)"
-    lines.setAttribute("src", "./images/lines in mobile.svg")   
+    lines.setAttribute("src", "./images/lines in mobile.svg")  
 }
